e2846eefcb19401a370bcc562b8a50015bdab487
HCK04 <meganx6s@gmail.com>
2024-06-13 14:31:24 +0100

    js project f commit

---
+++ java.js
@@ -0,0 +1,91 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// configuration des parametres
+let countItem = items.length;
+let itemActive = 0;
+// event clicker next
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem -1;
+    }
+    showSlider();
+}
+// autorun
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // supp item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // ajout active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// CONTACT US JAVASCRIPT!!//
+
+
+
+
+
+
+
+
+let form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
+
+
+function unsernameCheck(username) {
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(username);
+}
+
+
+
+
+
+

7b95ebb43ed873343e72caefae5ae0725d0f692b
HCK04 <meganx6s@gmail.com>
2024-06-28 11:42:49 +0100

    php

---
+++ php-logic/offre.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Services</title>
+	<link rel="stylesheet" href="port.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body class="bodyHome">
+	<header class="header">
+		<a href="#" class="logo">Akram.</a>
+		<nav class="navbar">
+			<a href="Portfolio.html" class="active">Home</a>
+			<a href="about.html" class="nl">About</a>
+			<a href="services.html" class="nl">Services</a>
+			<a href="portf.html" class="nl">Portfolio</a>
+			<a href="contact.html" class="nl">Contact</a>
+            <a href="offre.php" class="nl">Offre Travail</a>
+	</header>
+</body>
+<php
+
+
+
+?php>
